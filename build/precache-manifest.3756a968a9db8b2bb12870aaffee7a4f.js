2ee9a9ef699634f4663c31626a690444c769a306
NazarLiberty <NazarLiberty@gmail.com>
2020-07-02 00:11:58 +0300

    re-commit

---
+++ build/precache-manifest.3756a968a9db8b2bb12870aaffee7a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfab1676650dd2010e2acc8e99f399f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55e13bd49bb82fe4c646",
+    "url": "./static/css/main.dfa4bcde.chunk.css"
+  },
+  {
+    "revision": "78f531adf2959152dded",
+    "url": "./static/js/2.bf292ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bf292ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e13bd49bb82fe4c646",
+    "url": "./static/js/main.f76a027e.chunk.js"
+  },
+  {
+    "revision": "9aa3ef84936cc2c709fa",
+    "url": "./static/js/runtime-main.38bd2ffa.js"
+  }
+]);
